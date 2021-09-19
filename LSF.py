96632de59b87892218b6f630c556b08f67decb5d
Omar Alaa <81417777+omaralaahamdy@users.noreply.github.com>
2021-09-19 23:22:58 +0200

    Add files via upload

---
+++ LSF.py
@@ -0,0 +1,25 @@
+import numpy as np
+from numpy.linalg import inv, solve
+import matplotlib.pyplot as plt
+f=np.load("cases.npy")
+x=np.array(np.arange(0,39,1))
+r=[]
+c,t=[],0
+order=0 # modify this as you see fit
+plt.axvline(x=0,color='k'),plt.axhline(y=0,color='k'),plt.grid()
+#Least Square Fitting Model
+A=np.zeros(shape=(len(x),order+1))
+print(A)
+for i in range(order+1):
+    A[:,i]=x**i #A[start row:end row,start column:end column]
+print(A)
+a=solve(A.T.dot(A),A.T.dot(f)) #find factors of the polynomial using LSF
+for i in range(len(x)):
+    d=A[i,:].dot(a)
+    c.append(d)
+    plt.scatter(i,sum(c),color='red')
+plt.plot(x,np.cumsum(f),label="True Values")
+plt.legend()
+for i,j in enumerate(list(a)):
+    print('a{}={}'.format(i,j))
+plt.show()
